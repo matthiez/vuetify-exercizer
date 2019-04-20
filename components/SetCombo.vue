4c2aac3ddd8b4d726883edade77e68adb776cbcf
Andre Matthies <andre@ecolos.de>
2019-04-20 08:33:14 +0200

    init

---
+++ components/SetCombo.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <v-container grid-list-md>
+            <v-layout row wrap>
+                <v-flex
+                        fill-height
+                        d-flex
+                        xs12
+                        v-for="(combo, comboIndex) in combos"
+                        :key="comboIndex"
+                        @click="onSetCombo(comboIndex)"
+                        :style="{cursor: 'grab', border: comboIndex % 2 === 0 ? '1px solid #fff' : 'none'}">
+                    <v-layout row wrap>
+                        <v-flex d-flex xs6
+                                v-for="(bodyPart, bodyPartIndex) in combo"
+                                :key="bodyPartIndex">
+                            <v-list subheader>
+                                <v-subheader class="justify-center">
+                                    {{$t(weekdays[bodyPartIndex])}}
+                                </v-subheader>
+
+                                <v-list-tile text-center
+                                             v-for="bp in bodyPart"
+                                             :key="bp.text">
+                                    <v-list-tile-content>
+                                        <v-list-tile-title class="text-xs-center">
+                                            {{$t(bp.text)}}
+                                        </v-list-tile-title>
+                                    </v-list-tile-content>
+                                </v-list-tile>
+                            </v-list>
+                        </v-flex>
+                    </v-layout>
+                </v-flex>
+            </v-layout>
+        </v-container>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            combos: {
+                type: Array,
+                default: () => []
+            },
+            weekdays: {
+                type: Array,
+                default: () => []
+            }
+        },
+
+        methods: {
+            onSetCombo(comboIndex) {
+                this.$emit('step3', {comboIndex});
+            },
+        },
+    };
+</script>
---
+++ components/SetSplit.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-layout row wrap>
+        <v-hover v-for='(s, i) in $root.$exercizer.SPLITS' :key='i'>
+            <v-flex
+                    :class="`elevation-${hover ? 12 : 2}`"
+                    text-xs-center
+                    slot-scope="{ hover }">
+                <v-btn flat @click="onClickSplit(i)">{{s.text}}<br>{{s.subheading}}</v-btn>
+            </v-flex>
+        </v-hover>
+
+        <v-flex xs12 class='mt-4' text-xs-center>
+            <p>
+                {{$t('exercizer.setSplit.chooseSplit')}}<br>
+                {{$t('exercizer.setSplit.splitExample')}}<br>
+                {{$t('exercizer.setSplit.areYouSure')}}
+            </p>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+    export default {
+        methods: {
+            onClickSplit(splitIndex) {
+                this.$emit('step1', {splitIndex});
+            },
+        },
+    };
+</script>
